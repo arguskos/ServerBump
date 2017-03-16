3ca4d135d7b745add650c9b3f41e463020aec00b
arguskos <mark.furman@student.howest.be>
2017-03-16 12:56:46 +0400

    Backup

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -6,13 +6,21 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Random = UnityEngine.Random;
 
-
-public class Team
+public enum RoomsID
 {
-    public int  RoomNumber { get; set; }
-    public int teamId { get; set; }
-    public int ScoreRoom { get; set; }
-    public int ScoreAll { get; set; }
+    Debref=9999,
+    Bref=9998,
+    Between=9997,
+    Game=9996
+}
+public class Team
+{ 
+    public string TeamName;
+    public int RoomNumber;
+    public int TeamId;
+    public int ScoreRoom;
+    public float RoomTime;
+    public int ScoreAll;
 }
 
 public class Room
@@ -20,15 +28,24 @@ public class Room
     public int RoomTime;
     
 }
+
 public class Main : MonoBehaviour
 {
     private Room _currentRoom;
     public Text RoomTimer;
+    public Text Info;
+    public InputField InputField;
     public Text Score;
-    string qrString = "";
-    bool background = true;
-    private Team t = new Team();
-    private int lastTime;
+    private float _localTimer;
+    private string qrString = "";
+    private bool background = true;
+    private Team t;
+    private bool _enteredRoom = false;
+    private float _serverEllapsed = 0;
+
+
+
+
 
     public void IncScore()
     {
@@ -41,64 +58,266 @@ public class Main : MonoBehaviour
 
        
 
-        t.teamId = PlayerPrefs.GetInt("teamId",0); ;
-        if (t.teamId == 0)
-        {
-            t.teamId = Random.Range(0, 333);
-        }
-        PlayerPrefs.SetInt("teamId",t.teamId);
-        t.RoomNumber =0;
+      
+    }
+
+    public void CreateTeam()
+    {
+        t=new Team();
+
+        //t.teamId = PlayerPrefs.GetInt("teamId", 0); ;
+        //if (t.teamId == 0)
+        //{
+        //    t.teamId = Random.Range(0, 333);
+        //}
+        //PlayerPrefs.SetInt("teamId", t.teamId);
         string url = "http://192.168.0.151:8000/";
-        
+
         WWWForm form = new WWWForm();
-        form.AddField("", t.RoomNumber);
-        form.AddField("", t.teamId);
+        form.AddField("", "bla");
         byte[] rawData = form.data;
         var headers = new Dictionary<string, string>();
 
         headers.Add("Stage", "TeamCreation");
+        headers.Add("Name", InputField.text);
         // headers.Add("X-Auth-Token", "7da4596d42e24f9798d73ec40bbbbd81");
 
         Debug.Log("opj");
-        WWW www = new WWW(url, rawData,headers);
+        WWW www = new WWW(url, rawData, headers);
 
-        StartCoroutine(WaitForRequest(www));
+        StartCoroutine(RequestTeamCreation(www));
     }
 
+   
+    public void EnterRoom()
+    {
+        string url = "http://192.168.0.151:8000/";
+
+        WWWForm form = new WWWForm();
+        form.AddField("", "bla");
+        byte[] rawData = form.data;
+        var headers = new Dictionary<string, string>();
+
+        headers.Add("Stage", "RoomEntering");
+        headers.Add("TeamId", t.TeamId.ToString());
+        // headers.Add("X-Auth-Token", "7da4596d42e24f9798d73ec40bbbbd81");
+
+        WWW www = new WWW(url, rawData, headers);
+
+        StartCoroutine(RoomEnterRequest(www));
+
+
+
+    }
+
+    public void LeaveRoom()
+    {
+        string url = "http://192.168.0.151:8000/";
+
+        WWWForm form = new WWWForm();
+        form.AddField("", "bla");
+        byte[] rawData = form.data;
+        var headers = new Dictionary<string, string>();
+        t.ScoreRoom = int.Parse(Score.text);
+        headers.Add("Stage", "RoomFinished");
+        headers.Add("TeamId", t.TeamId.ToString());
+        headers.Add("RoomNumber", t.RoomNumber.ToString());
+        Debug.Log("Team:" +t.TeamId.ToString()+" leaving room with score:" +t.ScoreRoom.ToString() );
+        headers.Add("RoomScore",t.ScoreRoom.ToString());
+        
+        // headers.Add("X-Auth-Token", "7da4596d42e24f9798d73ec40bbbbd81");
+
+        WWW www = new WWW(url, rawData, headers);
+
+        StartCoroutine(RoomEndRequest(www));
+
+
+    }
+    IEnumerator GetTimeRequest(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            //   Debug.Log("WWW Ok!: " + www.data);
+
+            //t = JsonUtility.FromJson<Team>(www.data);
+            //Debug.Log(t.TeamName);
+            //Info.text = t.TeamName + " is in room:" + (t.RoomNumber) + " \nID:" + t.TeamId + " CurrScore" + t.ScoreRoom +
+            //            " All scores" + t.ScoreAll;
+            //Debug.Log(t.TeamId+"----"+t.RoomNumber.ToString());
+            // print(float.Parse( www.data));
+            //print(www.responseHeaders+"SAASDO{{SAD{SA");
+            foreach (var resp in www.responseHeaders)
+            {
+                print(resp.Key+""+resp.Value);
+            }
+           
+            // Debug.Log(r.RoomTime);
+            // t.RoomTime = r.RoomTime;
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
     void Update()
     {
-        if (_currentRoom!=null)
+
+        _localTimer +=Time.deltaTime;
+        if (_localTimer >= 2 && t!=null)
         {
-            if (_currentRoom.RoomTime > 0)
+            _localTimer = 0;
+            string url = "http://192.168.0.151:8000/";
+            var headers = new Dictionary<string, string>();
+            headers.Add("GetTime", "True");
+
+            WWW www = new WWW(url, null, headers);
+
+            StartCoroutine(GetTimeRequest(www));
+            Debug.Log("ROOM TIME AFTER COURUTINE "+t.RoomTime);
+            if (t != null)
             {
-                _currentRoom.RoomTime -= ((int)Time.time - lastTime)* 1000;
-                lastTime = (int) Time.time;
-                RoomTimer.text = (_currentRoom.RoomTime / 1000).ToString();
-                if (_currentRoom.RoomTime <= 0)
+                if (t.RoomTime > 0)
                 {
-                    string url = "http://192.168.0.151:8000/";
-                    Debug.Log("RoomFinished localy");
-                    WWWForm form = new WWWForm();
-                    t.ScoreRoom = int.Parse(Score.text);
-                    form.AddField("", t.teamId);
+                    //t.RoomTime -= (int)_serverEllapsed;
+                    Debug.Log(_serverEllapsed);
+                    RoomTimer.text = t.RoomTime.ToString();
+                    if (t.RoomTime <= 0)
+                    {
+                        RoomTimer.text = "0";
+                        LeaveRoom();
+                    }
+                    //_currentRoom.RoomTime -= ((int)Time.time - lastTime)* 1000;
+                    //lastTime = (int) Time.time;
+                    //RoomTimer.text = (_currentRoom.RoomTime / 1000).ToString();
+                    //if (_currentRoom.RoomTime <= 0)
+                    //{
+                    //    string url = "http://192.168.0.151:8000/";
+                    //    Debug.Log("RoomFinished localy");
+                    //    WWWForm form = new WWWForm();
+                    //    t.ScoreRoom = int.Parse(Score.text);
+                    //    form.AddField("", "bla");
 
-                    byte[] rawData = form.data;
-                    var headers = new Dictionary<string, string>();
-                    
-                    headers.Add("Stage", "RoomFinished");
-                    headers.Add("RoomScore",t.ScoreRoom.ToString());
-                    headers.Add("TeamId",t.teamId.ToString());
-                    headers.Add("RoomNumber", t.RoomNumber.ToString());
+                    //    byte[] rawData = form.data;
+                    //    var headers = new Dictionary<string, string>();
 
-                    // headers.Add("X-Auth-Token", "7da4596d42e24f9798d73ec40bbbbd81");
+                    //    headers.Add("Stage", "RoomFinished");
+                    //    headers.Add("RoomScore",t.ScoreRoom.ToString());
+                    //    headers.Add("TeamId",t.TeamId.ToString());
+                    //    headers.Add("RoomNumber", t.RoomNumber.ToString());
 
-                    WWW www = new WWW(url, rawData, headers);
-                    StartCoroutine(WaitForRequest(www));
+                    //    // headers.Add("X-Auth-Token", "7da4596d42e24f9798d73ec40bbbbd81");
+                    //    _enteredRoom = false;
+                    //    WWW www = new WWW(url, rawData, headers);
+                    //    StartCoroutine(WaitForRequest(www));
 
+                    //}
                 }
             }
         }
+
+        
     }
+    IEnumerator RoomEnterRequest(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.data);
+
+            //t = JsonUtility.FromJson<Team>(www.data);
+            //Debug.Log(t.TeamName);
+            //Info.text = t.TeamName + " is in room:" + (t.RoomNumber) + " \nID:" + t.TeamId + " CurrScore" + t.ScoreRoom +
+            //            " All scores" + t.ScoreAll;
+            //Debug.Log(t.TeamId+"----"+t.RoomNumber.ToString());
+            var temp = JsonUtility.FromJson<Team>(www.data);
+            
+            Debug.Log("REQUEST team name:"+temp.TeamName+" team id"+temp.TeamId+ "\n " +temp.RoomTime);
+            if (temp != null)
+            {
+                t = temp;
+            }
+
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+
+
+    IEnumerator GetTeamInfoRequest(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.data);
+
+            //t = JsonUtility.FromJson<Team>(www.data);
+            //Debug.Log(t.TeamName);
+            //Info.text = t.TeamName + " is in room:" + (t.RoomNumber) + " \nID:" + t.TeamId + " CurrScore" + t.ScoreRoom +
+            //            " All scores" + t.ScoreAll;
+            //Debug.Log(t.TeamId+"----"+t.RoomNumber.ToString());
+            t = JsonUtility.FromJson<Team>(www.data);
+            Debug.Log(t.TeamName);
+            Info.text = t.TeamName + " is in room:" + (t.RoomNumber) + " \nID:" + t.TeamId + " CurrScore" + t.ScoreRoom +
+                        " All scores" + t.ScoreAll;
+
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+    IEnumerator RoomEndRequest(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.data);
+
+            t = JsonUtility.FromJson<Team>(www.data);
+            Debug.Log("End room Request team Id:" +t.TeamId);
+            Info.text = t.TeamName + " is in room:" + (t.RoomNumber) + " \nID:" + t.TeamId + " CurrScore" + t.ScoreRoom +
+                        " All scores" + t.ScoreAll;
+            //Debug.Log(t.TeamId+"----"+t.RoomNumber.ToString());
+
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+    IEnumerator RequestTeamCreation(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.data);
+
+            t = JsonUtility.FromJson<Team>(www.data);
+ 
+            Info.text = t.TeamName + " is in room:" + (t.RoomNumber) + " \nID:" + t.TeamId + " CurrScore" + t.ScoreRoom +
+                        " All scores" + t.ScoreAll;
+            //Debug.Log(t.TeamId+"----"+t.RoomNumber.ToString());
+
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+
     IEnumerator WaitForRequest(WWW www)
     {
         yield return www;
@@ -155,7 +374,7 @@ public class Main : MonoBehaviour
                 t.RoomNumber = Int32.Parse(qrString);
                 WWWForm form = new WWWForm();
                 form.AddField("", t.RoomNumber);
-                form.AddField("", t.teamId);
+                form.AddField("", t.TeamId);
                 byte[] rawData = form.data;
                 var headers = new Dictionary<string, string>();
                 headers.Add("Stage", "RoomEntery");
--- Assets/Sender.cs
+++ Assets/Sender.cs
@@ -10,21 +10,19 @@ public class Sender : MonoBehaviour
     public Text tesd;
     void Start()
     {
-        t.teamId = 439243;
-        t.RoomNumber = 0;
-        string url = "http://192.168.0.151:8000/";
+       // t.RoomNumber = 0;
+       // string url = "http://192.168.0.151:8000/";
 
-        WWWForm form = new WWWForm();
-        form.AddField("",t.RoomNumber);
-        form.AddField("",t.teamId );
-        byte[] rawData = form.data;
-        var headers = new Dictionary<string, string>();
-        //headers.Add("User-Agent", "ceilometerclient.openstack.common.apiclient");
-       // headers.Add("X-Auth-Token", "7da4596d42e24f9798d73ec40bbbbd81");
+       // WWWForm form = new WWWForm();
+       // form.AddField("",t.RoomNumber);
+       // byte[] rawData = form.data;
+       // var headers = new Dictionary<string, string>();
+       // //headers.Add("User-Agent", "ceilometerclient.openstack.common.apiclient");
+       //// headers.Add("X-Auth-Token", "7da4596d42e24f9798d73ec40bbbbd81");
 
      
-        WWW www = new WWW(url,rawData,headers);
-        StartCoroutine(WaitForRequest(www));
+       // WWW www = new WWW(url,rawData,headers);
+       // StartCoroutine(WaitForRequest(www));
 
     }
 
